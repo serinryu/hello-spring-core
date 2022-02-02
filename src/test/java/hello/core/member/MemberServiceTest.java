7c72a6d72bd633f816cc2cc9d9111bba8c3b63af
serinryu <serin0404@naver.com>
2022-02-02 23:12:31 +0900

    AppConfig 생성 및 스프링 컨테이너 사용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach  //테스트 실행하기 전에 AppConfig에서 memberService 가져와서 위 memberService 에 할당을 해준다.
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
